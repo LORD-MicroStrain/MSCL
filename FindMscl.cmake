9e9b4d947e31d01dc8a3ba2119500febaf65ac37
Leonard Gerard <leonard@abundantrobotics.com>
2017-08-16 15:56:02 -0700

    cmake build and install making it easy as an externalproject

---
+++ FindMscl.cmake
@@ -0,0 +1,17 @@
+
+
+find_library(MSCL_LIB NAMES mscl)
+find_path(MSCL_INCLUDE NAMES mscl/Version.h)
+
+find(Boost REQUIRED COMPONENTS system filesystem)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Mscl DEFAULT_MSG MSCL_LIB MSCL_INCLUDE)
+
+add_library(Mscl::mscl SHARED IMPORTED)
+
+set_target_properties(Mscl::mscl PROPERTIES
+  IMPORTED_LOCATION ${MSCL_LIB}
+  INTERFACE_LINK_LIBRARIES "Boost::system;Boost::filesystem"
+  INTERFACE_INCLUDE_DIRECTORIES ${MSCL_INCLUDE})
+
