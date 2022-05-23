83353150fd1360561b412f7f61e54280a63ad25e
Nick DaCosta <nick.dacosta@parker.com>
2022-05-23 09:47:52 -0400

    PR changes
    Renamed InertialFieldParser_System as Mip
    Added force library include for system data parser
    Added system data to MipPacket::isDataPacket()
    Removed MipSystemFieldParser class (not needed)

---
+++ MSCL/source/mscl/MicroStrain/MIP/Packets/MipFieldParser_System.h
@@ -0,0 +1,14 @@
+/*****************************************************************************************
+**          Copyright(c) 2015-2022 Parker Hannifin Corp. All rights reserved.           **
+**                                                                                      **
+**    MIT Licensed. See the included LICENSE.txt for a copy of the full MIT License.    **
+*****************************************************************************************/
+
+#pragma once
+
+#include "mscl/MicroStrain/MIP/Packets/MipFieldParser.h"
+
+namespace mscl
+{
+    //Title: MipFieldParser_System
+}
