5b6b7ce86d8b0cc9d0b8d02306230b5f09bb3582
Nick DaCosta <nick.dacosta@hbkworld.com>
2024-12-03 11:38:57 -0500

    Fixed more Windows paths

--- .devcontainer/docker_build_win.ps1
+++ .devcontainer/docker_build_win.ps1
@@ -80,7 +80,7 @@ try {
     "${image_name}" -Command " `
       git config --global --add safe.directory C:/projects/mscl; `
       git fetch origin --tags; `
-      & 'C:/Projects/MSCL/BuildScripts/build_win.ps1' -branch ${env:BRANCH_NAME} -arch ${cmake_arch} -buildDir C:\projects\mscl\build_windows_${arch} ${python2_build_script_flags} ${python3_build_script_flags}; `
+      & 'C:/Projects/MSCL/BuildScripts/build_win.ps1' -branch ${env:BRANCH_NAME} -arch ${cmake_arch} -buildDir C:/projects/mscl/build_windows_${arch} ${python2_build_script_flags} ${python3_build_script_flags}; `
     "
 }
 catch {
