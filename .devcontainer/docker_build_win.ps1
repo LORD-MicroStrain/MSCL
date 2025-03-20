c2834a7bea5a7eaeb8e4a4a8ea9cf725023c4905
Nick DaCosta <nick.dacosta@hbkworld.com>
2025-03-20 14:55:56 -0400

    Omitting rethrowing of exception in Docker PowerShell script

--- .devcontainer/docker_build_win.ps1
+++ .devcontainer/docker_build_win.ps1
@@ -104,11 +104,6 @@ try
             git fetch origin --tags; `
             & 'C:/Projects/MSCL/BuildScripts/build_win.ps1' -branch ${env:BRANCH_NAME} -arch ${cmake_arch} -buildDir C:/projects/mscl/build_windows_${arch} ${python2_build_script_flags} ${python3_build_script_flags}; `
         "
-
-    if (-Not ($?))
-    {
-        throw $_
-    }
 }
 finally
 {
